adaaffaf2b47b59327e2382f39fb737917ac6d1d
Marvin Sequera <marvin.sequera@avirato.com>
2021-09-18 12:48:02 +0200

    Src folder created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
-const { typeDefs } = require('./Schema/TypeDefs')
-const { resolvers } = require('./Schema/Resolvers')
+const { typeDefs } = require('../Schema/TypeDefs')
+const { resolvers } = require('../Schema/Resolvers')
 const app = express()
 
 const  server = new ApolloServer({ typeDefs, resolvers })

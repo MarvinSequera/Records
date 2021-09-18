9df21e9411be0e036ed9fd268face5c37d494e39
Marvin Sequera <marvin.sequera@avirato.com>
2021-09-18 12:11:36 +0200

    Add TypeDefs for BBDD

--- server/Schema/TypeDefs.js
+++ server/Schema/TypeDefs.js
@@ -1,24 +1,53 @@
 const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
-    type Disc {
+    type Artist {
+        id_Artist: Int!
         name: String!
-        year: Int!
-        composer: String!
-        songs: [Song]
     }
 
-    type Song {
+    type Collection {
+        id: Int!
+        fk_record_id: Int!
+        fk_user_id: Int!
+    }
+
+    type Genres {
+        id_Genre: Int!
         name: String!
-        duration: String!
+    }
+
+    type Records {
+        id_Record: Int!
+        name: String!
+        year: Int!
+        fk_genre_id: Int!
+        fk_artist_id: Int!
+    }
+
+    type Record_Song {
+        id_Record_Song: Int!
+        fk_record_id: Int!
+        fk_song_id: Int!
+    }
+
+    type Songs {
+        id_Song: Int!
+        name: String!
+        year: Int!
+        fk_artist_id: Int!
+        fk_genre_id: Int!
+    }
+
+    type User {
+        id_User: Int!
+        email: String!
+        password: String!
+        active: Boolean!
     }
 
     #Queries
     
-    type Query {
-        getAllDiscs: [Disc]
-    }
-
     #Mutations
 `
 
